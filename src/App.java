6577f9598e4bb448e7c1b26b7d05692353fb0dac
João Gabriel <jgmpages@gmai.com>
2026-05-16 17:36:33 -0300

    Initial commit for Flappy Bird portfolio project

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int LarguraBorda = 360;
+        int AlturaBorda = 640;
+        JFrame janela = new JFrame("Flappy Bird");
+        janela.setSize(LarguraBorda, AlturaBorda);
+        janela.setVisible(true);
+        janela.setLocationRelativeTo(null);
+        janela.setResizable(false);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
